3fb14cf88d2a42f32db4c5f916f8a7259835cc4c
Landon Bouma <landonb@users.noreply.github.com>
2024-08-24 03:00:08 -0500

    Refactor: Move related functions to new lib file

--- lib/dep_apply_confirm_patch_base.sh
+++ lib/dep_apply_confirm_patch_base.sh
@@ -173,67 +173,6 @@ choose_patch_base_or_ask_user () {
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-# RELATED: identify_scope_ends_at
-determine_scoping_boundary () {
-  local patch_branch="$1"
-
-  local scoping_starts_at=""
-
-  local private_scope_starts_at
-  private_scope_starts_at="$(find_oldest_commit_by_message "^${PRIVATE_PREFIX}")"
-
-  local protected_scope_starts_at
-  protected_scope_starts_at="$(find_oldest_commit_by_message "^${SCOPING_PREFIX}")"
-
-  # If patches from 'release' but there's only 'private' locally, then
-  # also exclude earlier protected-prefix commits from git-am, to avoid
-  # conflicts, and because the remote will not have included these.
-  if [ "${patch_branch}" != "${LOCAL_BRANCH_PRIVATE}" ]; then
-    scoping_starts_at="${protected_scope_starts_at}"
-  fi
-
-  if [ -z "${scoping_starts_at}" ]; then
-    scoping_starts_at="${private_scope_starts_at}"
-  fi
-
-  if [ -z "${scoping_starts_at}" ]; then
-    scoping_starts_at="HEAD"
-  fi
-
-  printf "${scoping_starts_at}"
-}
-
-# ***
-
-# RELATED: determine_scoping_boundary
-#
-# CPYST:
-#   . ~/.kit/git/git-put-wise/deps/sh-git-nubs/lib/git-nubs.sh
-#   . ~/.kit/git/git-put-wise/lib/common_put_wise.sh
-#   . ~/.kit/git/git-put-wise/lib/dep_apply_confirm_patch_base.sh
-#   . ~/.kit/git/git-put-wise/lib/put_wise_push_remotes.sh
-#   identify_scope_ends_at "^${PRIVATE_PREFIX}"
-#   identify_scope_ends_at "^${SCOPING_PREFIX}" "^${PRIVATE_PREFIX}"
-identify_scope_ends_at () {
-  local scope_ends_at=""
-
-  for message_re in "$@"; do
-    scope_ends_at="$(find_oldest_commit_by_message "${message_re}")"
-
-    [ -z "${scope_ends_at}" ] || break
-  done
-
-  if [ -z "${scope_ends_at}" ]; then
-    scope_ends_at="HEAD"
-  else
-    scope_ends_at="${scope_ends_at}^"
-  fi
-
-  printf "${scope_ends_at}"
-}
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
-
 suss_which_known_branch_is_furthest_along () {
   local patch_base="$1"
 
