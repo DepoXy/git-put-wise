DepoXy/git-put-wise

commit 0b57235729463b21afc92d671b0e884746bc8534

Feature: Treat feature branches more specially on push
- A feature branch will now push local 'release' to the
  remote 'release' branch, if both are ancestors of the
  feature branch, and it will not move 'release'.

  - This contrasts with push from 'private' branch,
    which will move 'private' to the scoping boundary
    if a local 'private' branch exists. And it'll
    push the scoping boundary to a 'release' remote
    if one exists.

- A feature branch will now push protected commits to
  an 'entrust' remote, if one is named.

  - While push from 'release' or 'private' always uses
    the 'entrust/scoping' upstream, a feature branch uses
    the 'entrust/<branch>' upstream (so if you made a
    local 'scoping' branch and pushed that, you might
    push to the same upstream as the special branches).

- The feature branch will still uses its remote branch
  as the rebase boundary. But if there's no remote branch,
  the boundary will fallback on a local 'release' or
  remote 'release' or 'scoping' branches. In the unlikely
  event those exist, I suppose.

lib/put_wise_push_remotes.sh
@@ -88,7 +88,7 @@ put_wise_push_remotes_go () {
   )"
 
   local release_boundary_or_HEAD="${scoping_boundary_or_HEAD}"
-  if [ -n "${remote_current}" ]; then
+  if [ -n "${remote_name}" ]; then
     # Current branch is a feature branch (not 'release' or 'private').
     # - In this workflow, user self-manages the local 'release' pointer,
     #   which we'll push if ahead of publish/release. But we won't move
@@ -248,30 +248,12 @@ bind generic r +<sh -c \" \\
       >&2 echo "${PW_USER_CANCELED_GOODBYE}"
     fi
 
-    if ${keep_going} && ! ${restrict_release}; then
-      if prompt_user_to_continue_push_remote_branch ${keep_going} "${remote_protected}"; then
-        echo_announce_push "${SCOPING_REMOTE_BRANCH}"
-        ${DRY_ECHO} git push "${SCOPING_REMOTE_NAME}" \
-          "${protected_boundary_or_HEAD}:refs/heads/${SCOPING_REMOTE_BRANCH}" ${git_push_force} \
-            || handle_push_failed "${SCOPING_REMOTE_NAME}/${SCOPING_REMOTE_BRANCH}"
-      fi
-
-      if prompt_user_to_continue_push_remote_branch ${keep_going} "${remote_current}"; then
-        echo_announce_push "${branch_name}"
-        ${DRY_ECHO} git push "${remote_name}" \
-          "${scoping_boundary_or_HEAD}:refs/heads/${branch_name}" ${git_push_force} \
-            || handle_push_failed "${remote_name}/${branch_name}"
-      fi
+    if ! ${restrict_release}; then
+      prompt_push "${remote_protected}" "${protected_boundary_or_HEAD}" ${git_push_force}
+      prompt_push "${remote_current}" "${scoping_boundary_or_HEAD}" ${git_push_force}
     fi
 
-    if ${keep_going}; then
-      if prompt_user_to_continue_push_remote_branch ${keep_going} "${remote_release}"; then
-        echo_announce_push "${RELEASE_REMOTE_BRANCH}"
-        ${DRY_ECHO} git push "${RELEASE_REMOTE_NAME}" \
-          "${release_boundary_or_HEAD}:refs/heads/${RELEASE_REMOTE_BRANCH}" ${git_push_force} \
-            || handle_push_failed "${RELEASE_REMOTE_NAME}/${RELEASE_REMOTE_BRANCH}"
-      fi
-    fi
+    prompt_push "${remote_release}" "${release_boundary_or_HEAD}" ${git_push_force}
   fi
 
   if ! ${keep_going}; then
@@ -296,6 +278,30 @@ bind generic r +<sh -c \" \\
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+prompt_push () {
+  local upstream_remote="$1"
+  local boundary_or_HEAD="$2"
+  local git_push_force="$3"
+
+  if ! prompt_user_to_continue_push_remote_branch \
+    ${keep_going} "${upstream_remote}"\
+  ; then
+
+    return 0
+  fi
+
+  local remote_name
+  local branch_name
+  remote_name="$(git_upstream_parse_remote_name "${upstream_remote}")"
+  branch_name="$(git_upstream_parse_branch_name "${upstream_remote}")"
+
+  echo_announce_push "${branch_name}"
+
+  ${DRY_ECHO} git push "${remote_name}" \
+    "${boundary_or_HEAD}:refs/heads/${branch_name}" ${git_push_force} \
+      || handle_push_failed "${remote_name}/${branch_name}"
+}
 
 echo_announce_push () {
   local branch="$1"

lib/dep_rebase_boundary.sh
@@ -83,12 +83,14 @@ put_wise_identify_rebase_boundary_and_remotes () {
 
   local applied_tag="$(format_pw_tag_applied "${branch_name}")"
 
+  local is_hyper_branch=false
   if [ "${branch_name}" = "${LOCAL_BRANCH_PRIVATE}" ] \
     || [ "${branch_name}" = "${LOCAL_BRANCH_RELEASE}" ] \
   ; then
-    local_release="${LOCAL_BRANCH_RELEASE}"
-    remote_release="${REMOTE_BRANCH_RELEASE}"
-    remote_protected="${REMOTE_BRANCH_SCOPING}"
+    is_hyper_branch=true
+  fi
+
+# ***
 
     # The pw/in tag signifies the final patch from the latest --apply command.
     # It's the remote's HEAD, essentially (minus PRIVATE commits). This is the
@@ -180,17 +182,20 @@ put_wise_identify_rebase_boundary_and_remotes () {
 
     local remote_ref=""
 
+    local scoping_branch="${SCOPING_REMOTE_BRANCH}"
+    ${is_hyper_branch} || scoping_branch="${branch_name}"
+
     if remote_ref="$( \
       fetch_and_check_branch_exists_or_remote_online \
         "${SCOPING_REMOTE_NAME}" \
-        "${SCOPING_REMOTE_BRANCH}" \
+        "${scoping_branch}" \
     )"; then
+      remote_protected="${SCOPING_REMOTE_NAME}/${scoping_branch}"
+      # Prefer pw/in over scoping boundary
       if [ -z "${rebase_boundary}" ]; then
-        # May be empty string if remote exists and remote branch absent (first push).
+        # Might be empty string if remove exists but not branch (first push).
         rebase_boundary="${remote_ref}"
       fi
-    else
-      remote_protected=""
     fi
 
     # Prefer sorting from local or remote 'release' branch.
@@ -199,39 +204,58 @@ put_wise_identify_rebase_boundary_and_remotes () {
         "${RELEASE_REMOTE_NAME}" \
         "${RELEASE_REMOTE_BRANCH}" \
     )"; then
+      remote_release="${REMOTE_BRANCH_RELEASE}"
       # May be empty string if remote exists and remote branch absent (first push).
       rebase_boundary="${remote_ref}"
-    else
-      remote_release=""
     fi
 
-    if [ "${branch_name}" = "${LOCAL_BRANCH_PRIVATE}" ]; then
-      if git_branch_exists "${local_release}"; then
-        rebase_boundary="${local_release}"
-      else
-        local_release=""
+    if [ "${branch_name}" != "${LOCAL_BRANCH_RELEASE}" ]; then
+      if git_branch_exists "${LOCAL_BRANCH_RELEASE}"; then
+        local_release="${LOCAL_BRANCH_RELEASE}"
+        rebase_boundary="${LOCAL_BRANCH_RELEASE}"
       fi
     elif [ "${branch_name}" = "${LOCAL_BRANCH_RELEASE}" ]; then
+      local_release="${LOCAL_BRANCH_RELEASE}"
       if git_branch_exists "${LOCAL_BRANCH_PRIVATE}"; then
         warn "ALERT: Working from branch '${LOCAL_BRANCH_RELEASE}'," \
           "but '${LOCAL_BRANCH_PRIVATE}' branch also exists"
       fi
     fi
 
-    # else, if rebase_boundary unset, will die after return from if-block.
-
     if [ -n "${remote_release}" ] && [ -n "${local_release}" ]; then
       # Verify 'release/release' is at or behind 'release'.
       local divergent_ok=false
 
+      # Exits on error.
       must_confirm_commit_at_or_behind_commit "${remote_release}" "${local_release}" \
         ${divergent_ok} "remote-release" "local-release"
 
-      if [ "${branch_name}" = "${LOCAL_BRANCH_PRIVATE}" ]; then
-        # Rebase starting from 'release', which is guaranteed at or further
-        # along than 'publish/release'.
-        rebase_boundary="${local_release}"
+      if [ "${branch_name}" != "${LOCAL_BRANCH_RELEASE}" ]; then
+        if git merge-base --is-ancestor "${local_release}" "${branch_name}"; then
+          # Rebase starting from 'release', which is guaranteed at or further
+          # along than 'publish/release'.
+          rebase_boundary="${local_release}"
+        elif ${is_hyper_branch}; then
+          warn "ALERT: '${local_release}' not ancestor of '${branch_name}'"
+        else
+          # This is a feature branch, and there's no rule about 'release'
+          # being an ancestor. It's merely a courteousy/convenience that
+          # we support it.
+          local_release=""
+          remote_release=""
+        fi
       fi
+    elif ! ${is_hyper_branch}; then
+      # On push feature branch, 'release' only pushed to remote 'release',
+      # so if both don't exist nothing to do.
+      local_release=""
+      remote_release=""
+    fi
+
+    # On force-push feature branch, don't include 'release' branches.
+    if ${PW_OPTION_FORCE_PUSH:-false} && [ "${branch_name}" != "${LOCAL_BRANCH_RELEASE}" ]; then
+      local_release=""
+      remote_release=""
     fi
 
     # NOTE: If resorting since 'release' or 'publish/release', it means
@@ -242,10 +266,10 @@ put_wise_identify_rebase_boundary_and_remotes () {
 
   # fi: very long [ "${branch_name}" = "${LOCAL_BRANCH_PRIVATE}" ]
 
-  else
+    if ! ${is_hyper_branch}; then
     # ${branch_name} not 'release' or 'private'.
     # - Note that push.default defaults to 'simple', which pushes to upstream
-    #   tracking branch when pushing to that remote, otherwise works like
+    #   tracking branch when pushing to that remote. This code works like
     #   push.default 'current', which uses same name for pushing.
     #   - For feature branches, I like to track the trunk, so git-pull
     #     rebases appropriately. But I like push.default 'current', so
@@ -255,24 +279,22 @@ put_wise_identify_rebase_boundary_and_remotes () {
     tracking_branch="$(git_tracking_branch)" \
       || true
 
+    # Honor PW_OPTION_REMOTE, but not PW_OPTION_BRANCH (use branch_name).
     remote_name="${PW_OPTION_REMOTE}"
 
     if [ -z "${remote_name}" ] && [ -n "${tracking_branch}" ]; then
       remote_name="$(git_upstream_parse_remote_name "${tracking_branch}")"
     fi
 
-    # Note we don't use PW_OPTION_BRANCH here, but the current branch.
-    remote_current="${remote_name}/${branch_name}"
-
-    if git_remote_exists "${remote_name}"; then
-      >&2 echo_announce "Fetch from ‘${remote_name}’"
-
-      # MAYBE/2023-01-18: GIT_FETCH: Use -q?
-      git fetch "${remote_name}"
-
-      if git_remote_branch_exists "${remote_current}"; then
-        rebase_boundary="${remote_current}"
-      fi
+    if remote_ref="$( \
+      fetch_and_check_branch_exists_or_remote_online \
+        "${remote_name}" \
+        "${branch_name}" \
+    )"; then
+      # Note we don't use PW_OPTION_BRANCH here, but the current branch.
+      remote_current="${remote_name}/${branch_name}"
+      # Might be empty string if remove exists but not branch.
+      rebase_boundary="${remote_ref}"
     fi
   fi
 

commit 1f77be82b06376afc9564c2dbf5b5fcb272c3662

Bugfix: Revert push order so scoping pushed last, in case fails
- Fixes issue introduced 4 days ago by 0b57235:

    Feature: Treat feature branches more specially on push

lib/put_wise_push_remotes.sh
@@ -272,11 +272,16 @@ bind generic r +<sh -c \" \\
     fi
 
     if ! ${restrict_release}; then
-      prompt_push "${remote_protected}" "${protected_boundary_or_HEAD}" ${git_push_force}
       prompt_push "${remote_current}" "${scoping_boundary_or_HEAD}" ${git_push_force}
     fi
 
     prompt_push "${remote_release}" "${release_boundary_or_HEAD}" ${git_push_force}
+
+    # Push 'scoping' last, in case fails b/c diverged and sets keep_going=false
+    # (so at least pushes to 'release', which is probably more important to user).
+    if ! ${restrict_release}; then
+      prompt_push "${remote_protected}" "${protected_boundary_or_HEAD}" ${git_push_force}
+    fi
   fi
 
   if ! ${keep_going}; then